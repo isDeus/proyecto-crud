5c54359a605a4238d895dddb9eb5fb07bd7732a8
tunombre <m.campos14@ufromail.cl>
2022-08-02 12:21:57 -0400

    Se añadio estilos a botones y fotos

--- resources/views/empleado/form.blade.php
+++ resources/views/empleado/form.blade.php
@@ -2,31 +2,39 @@
 {{-- Se añaden los values en cada input para recibir (si es que existen) los valores y así poder mostrarlos --}}
 {{-- Se refactorizó para mostrar o no los valores si es que existen (con isset) --}}
 <h1>{{$modo}} empleado</h1>
+
+<div class="form-group">
 <label for="Nombre">Nombre</label>
-<input type="text" name="Nombre" value="{{ isset($empleado->Nombre)?$empleado->Nombre:''}}" id="Nombre">
-<br>
+<input type="text" name="Nombre" value="{{ isset($empleado->Nombre)?$empleado->Nombre:''}}" id="Nombre" class="form-control">
+</div>
 
+<div class="form-group">
 <label for="ApellidoPaterno">Apellido Paterno</label>
-<input type="text" name="ApellidoPaterno" value="{{ isset($empleado->ApellidoPaterno)?$empleado->ApellidoPaterno:''}}" id="ApellidoPaterno">
-<br>
+<input type="text" name="ApellidoPaterno" value="{{ isset($empleado->ApellidoPaterno)?$empleado->ApellidoPaterno:''}}" id="ApellidoPaterno" class="form-control">
+</div>
 
+<div class="form-group">
 <label for="ApellidoMaterno">Apellido Materno</label>
-<input type="text" name="ApellidoMaterno" value="{{ isset($empleado->ApellidoMaterno)?$empleado->ApellidoMaterno:''}}" id="ApellidoMaterno">
-<br>
+<input type="text" name="ApellidoMaterno" value="{{ isset($empleado->ApellidoMaterno)?$empleado->ApellidoMaterno:''}}" id="ApellidoMaterno" class="form-control">
+</div>
 
+<div class="form-group">
 <label for="Correo">Correo</label>
-<input type="text" name="Correo" value="{{ isset($empleado->Correo)?$empleado->Correo:''}}" id="Correo">
-<br>
+<input type="text" name="Correo" value="{{ isset($empleado->Correo)?$empleado->Correo:''}}" id="Correo" class="form-control">
+</div>
 
-<label for="Foto">Foto</label>
+<div class="form-group">
+<label for="Foto"></label>
 @if (isset($empleado->Foto))
-<img src="{{asset('storage').'/'.$empleado->Foto}}" width="100" alt="">
+<img src="{{asset('storage').'/'.$empleado->Foto}}" width="100" alt="" class="img-thumbnail img-fluid">
 @endif
-<input type="file" name="Foto" value="" id="Foto">
+<input type="file" name="Foto" value="" id="Foto" class="form-control">
+</div>
+
 <br>
 
-<input type="submit" value="{{$modo}} datos">
+<input type="submit" value="{{$modo}} datos" class="btn btn-success">
 
-<a href="{{url('empleado/')}}">Regresar</a>
+<a href="{{url('empleado/')}}" class="btn btn-primary">Regresar</a>
 
 <br>
